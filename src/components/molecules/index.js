de8730817c0a8af81ba9ef3d3af6c795b3d22233
den <den.velez@gmail.com>
2020-08-22 17:25:57 -0500

    file're fixed after pull origin components(second)

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,15 +1,12 @@
 export * from "./ButtonList";
 export * from "./ImageMore";
 export * from "./MenuItem";
-<<<<<<< HEAD
 export * from "./InfoButtons";
 export * from "./Upload";
 export * from "./SaveNext";
 export * from "./ImageTitle";
-=======
 export * from "./MenuItemSmall";
 export * from "./IconName";
 export * from "./IconCard";
 export * from "./Github";
 export * from "./OrganizerCard";
->>>>>>> 71ea33f2fb51acfaa4802d06faf9d521a3478ef7
