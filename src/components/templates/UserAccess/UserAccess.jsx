02e42f10c6feedc40d654dbc81f6385621e2e4ef
Héctor Reyes <h.reyes@idventiva.com>
2020-08-23 18:26:50 -0500

    Error in UserAccess

--- src/components/templates/UserAccess/UserAccess.jsx
+++ src/components/templates/UserAccess/UserAccess.jsx
@@ -4,13 +4,14 @@ import { Login, Register, Header } from "../../organisms";
 export function UserAccess({ page, buttons, background, src, alt }) {
   return (
     <>
-      <Header buttons={buttons} />
+      <h1>Hol</h1>
+      {/* <Header buttons={buttons} />
       <main className={`UserAccess ${background}`}>
         <figure>
           <img src={src} alt={alt} />
         </figure>
         {page === "login" ? <Login /> : <Register />}
-      </main>
+      </main> */}
     </>
   );
 }
