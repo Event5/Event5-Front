cefd66b59403f6c709c7e3bd1694cf119294e2a9
Héctor Reyes <h.reyes@idventiva.com>
2020-08-23 18:06:32 -0500

    Routes test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { BigLogo, ButtonAlt } from "../components/atoms";
+import { Link } from "react-router-dom";
 
 export function Home() {
   return (
     <>
       <BigLogo />
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
+      <Link to="/about">About</Link>
     </>
   );
 }
