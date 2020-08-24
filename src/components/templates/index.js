51a677b3ad0816792b8b4077eb3c5cd215b4821f
den <den.velez@gmail.com>
2020-08-24 17:08:54 -0500

    files were fixed after pull for github

--- src/components/templates/index.js
+++ src/components/templates/index.js
@@ -1,8 +1,5 @@
 export * from "./UserAccess";
 export * from "./Content";
-<<<<<<< HEAD
 export * from "./NotFound";
 export * from "./Home";
-=======
 export * from "./TeamContainer";
->>>>>>> 554da7b08c654466fcdef0dc8d48a7f5da226bd5
