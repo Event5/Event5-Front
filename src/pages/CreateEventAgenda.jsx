8d6450709b4ce4145345c3f6fafafa4d0900d241
Héctor Reyes <h.reyes@idventiva.com>
2020-08-26 08:17:57 -0500

    Agenda Page

--- src/pages/CreateEventAgenda.jsx
+++ src/pages/CreateEventAgenda.jsx
@@ -1,13 +1,56 @@
 import React from "react";
 
-import { SidebarMenu } from "../components/organisms/SidebarMenu";
+import { AppHeader, SaveNext } from "../components/molecules";
+import {
+  SidebarMenu,
+  SectionTitle,
+  ConferenceCard,
+} from "../components/organisms";
 import { Content } from "../components/templates/Content";
 
 export function CreateEventAgenda() {
   return (
     <main className="app-layout">
       <SidebarMenu />
-      <Content></Content>
+      <Content>
+        <AppHeader btnText="New Organization" />
+        <SectionTitle
+          title="Agenda: Event Name"
+          description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor."
+          btnText="New Event"
+          btnType="primary"
+          btnColor="light"
+        />
+        <ConferenceCard
+          sessionTitle="Conference number one"
+          date="18/08/2022"
+          duration="3:00"
+          description="Description of the number one conference."
+          BottomBtn="Conferencist Name"
+        />
+        <ConferenceCard
+          sessionTitle="Conference number one"
+          date="18/08/2022"
+          duration="3:00"
+          description="Description of the number one conference."
+          BottomBtn="Conferencist Name"
+        />
+        <ConferenceCard
+          sessionTitle="Conference number one"
+          date="18/08/2022"
+          duration="3:00"
+          description="Description of the number one conference."
+          BottomBtn="Conferencist Name"
+        />
+        <ConferenceCard
+          sessionTitle="Conference number one"
+          date="18/08/2022"
+          duration="3:00"
+          description="Description of the number one conference."
+          BottomBtn="Conferencist Name"
+        />
+        <SaveNext />
+      </Content>
     </main>
   );
 }
